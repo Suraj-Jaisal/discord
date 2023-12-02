e782611fda5d1da61ad334d9a5fb2b8a216b08ab
suraj jaisal <surajjaisal445@gmail.com>
2023-12-03 04:28:17 +0530

    create server sidebar

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -8,6 +8,7 @@ import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -28,6 +29,7 @@ export const ModalProvider = () => {
       <MembersModal />
       <CreateChannelModal />
       <DeleteServerModal />
+      <LeaveServerModal />
     </>
   );
 };
